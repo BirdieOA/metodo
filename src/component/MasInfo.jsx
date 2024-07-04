0fe94a6496d488d7967f442bb6760dfcc5da23de
BirdieOA <maitedeleonf@gmail.com>
2024-07-03 21:05:51 -0300

    mods

--- src/component/MasInfo.jsx
+++ src/component/MasInfo.jsx
@@ -15,12 +15,12 @@ export default function Curso() {
 
 	return (
 		<>
-			<button
+			{/* <button
 				className='rounded-md text-slate-700 lg:text-xl font-poppinsRegular hover:border-orange-300 hover:bg-orange-400 hover:text-slate-600 cursor-pointer ease-in duration-200 bottom-10 right-10 px-4 py-4 uppercase'
 				onClick={openModal}
 			>
 				Más Información
-			</button>
+			</button> */}
 
 			{isOpen && (
 				<div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-90 z-20 backdrop-blur-md' onClick={closeModal}>
